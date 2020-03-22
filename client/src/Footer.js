b88722fef9335b54ee35db2d92d8af84bb1d32c5
Lee Peters <lpeters0023@gmail.com>
2020-03-22 17:09:10 -0400

    server back

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -5,16 +5,16 @@ function Footer() {
   return(
     <div id='footer'>
       <Link id='footerLink' to={'/'}>
-        <a href='mailto: lpeters0023@gmail.com'>
+        <a href='mailto: lpeters0023@gmail.com?Subject=Hello!'target="_blank">
           <button id="footerButton">Say<strong> Hi</strong></button>
           </a>
       </Link>
       <div id='footerLogos'>
         <a href='https://linkedin.com/in/leepeters23/'>
-        <img src='linkedIn.png' alt='linkedIn logo' className='logo'></img>
+        <img src='Images/linkedIn.png' alt='linkedIn logo' className='logo'></img>
         </a>
         <a href='https://github.com/leepeters0023'>
-        <img src='github.png' alt='github logo' className='logo'></img>
+        <img src='Images/gtihub.png' alt='github logo' className='logo'></img>
         </a>
       </div>
 
--- client/src/WebDev.js
+++ client/src/WebDev.js
@@ -24,7 +24,7 @@ class WebDev extends React.Component {
     return (
       <div>
         <div id='webDev'>
-          <img onClick={this.openModal} src='https://placekitten.com/300/300'></img>
+          <img className='webDevThumb'onClick={this.openModal} src='Images/yelpington.png'></img>
         </div>
         {this.state.modalDisplay ? <TestModal closeModal={this.closeModal} /> : null}
       </div>
