1a423ebabf3a8691e54df4cc4ac7025c4acf1830
Lee Peters <lpeters0023@gmail.com>
2020-03-23 09:23:41 -0400

    heroku again

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,26 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Route } from 'react-router-dom' 
+import { Route } from 'react-router-dom'
 
-// if route does not = 'home', 'home' button is displayed in nav
 function Header() {
-  {!Route path={'/Home'?
   return (
     <div id="header">
       <div id='headerButtons'>
-        <Link to={'/WebDev'}>
-          <button className="headerButton">Web Dev</button>
-        </Link>
-        <Link to={'/design'}>
-          <button className="headerButton">Design</button>
-        </Link>
-      </div>
-    </div>
-  ) : 
-  return (
-    <div id="header">
-      <div id='headerButtons'>
-        {!Route path={'/Home'}}
         <Link to={'/'}>
           <button className="headerButton">Home</button>
         </Link>
@@ -33,7 +18,6 @@ function Header() {
       </div>
     </div>
   )
-  }
 }
 
 export default Header
