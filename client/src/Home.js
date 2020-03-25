c92e7caf6050289c1754f6e819a5136ec14311d6
Joshua Burke <burke.joshua.james@gmail.com>
2020-03-25 17:59:58 -0400

    fix CSS opacity problem

--- client/src/Home.js
+++ client/src/Home.js
@@ -3,22 +3,22 @@ import { Link } from 'react-router-dom'
 
 function Home(props) {
   return (
-    <div id='homeMain'>
-      <div id='homeLanding'>
-      <div id="homePortrait">
+    <div id='home-main'>
+      <div id='home-landing'>
+      <div id="home-portrait">
       <a href="https://imgur.com/fL7xqIK"><img src="https://i.imgur.com/fL7xqIK.jpg?1" title="source: imgur.com" /></a>
          </div>
          <div id='howdy'>
         <h1>Howdy</h1>
         </div>
       </div>
-      
-      <div id='homeAbout'>
+
+      <div id='home-about'>
         <h2>I'm Lee</h2>
-        <p id='aboutP'>Thanks for stopping by, I am a creative problem solver who enjoys using new technology to nerd out on ways to make things more efficient and look great. I’ve always sought the “Why” behind things and consider myself a lifelong learner in constant pursuit of perfecting my craft. When not coding or designing, I can be found in the mountains out for a run or ski depending on the season.  </p>
+        <p id='about-p'>Thanks for stopping by, I am a creative problem solver who enjoys using new technology to nerd out on ways to make things more efficient and look great. I’ve always sought the “Why” behind things and consider myself a lifelong learner in constant pursuit of perfecting my craft. When not coding or designing, I can be found in the mountains out for a run or ski depending on the season.  </p>
       </div>
 
-      <div id='homeSkills'>
+      <div id='home-skills'>
         <img id='owl'src='Images/owl.svg'></img>
         <div>
         <h2>Skills</h2>
@@ -46,11 +46,11 @@ function Home(props) {
           </div>
       </div>
 
-      <div id='homeCallToAction'>
+      <div id='home-call-to-action'>
         <h3>Let's build beautiful, meaningful things together</h3>
       </div>
     </div>
-  ) // link tags function the same way as anchor tags in that we must wrap things in them 
+  ) // link tags function the same way as anchor tags in that we must wrap things in them
 }
 
-export default Home; 
+export default Home;
--- client/src/index.js
+++ client/src/index.js
@@ -1,32 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Switch, Route, BrowserRouter } from 'react-router-dom' 
-import './App.css';
+import { BrowserRouter } from 'react-router-dom'
 import App from './App';
-import Home from './Home'
-import Header from './Header';
-import Design from './Design';
-import WebDev from './WebDev';
-import Footer from './Footer';
 import * as serviceWorker from './serviceWorker';
 
-function Router(props) {
-  return(
-    <BrowserRouter>
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path={'/'} component={App} />
-        <Route path={'/Home'} component={Home} />
-        <Route path={'/WebDev'} component={WebDev} />
-        <Route path={'/Design'} component={Design} />
-        <Route path={'/Footer'} component={Footer} />
-      </Switch>
-      </div>
-    </BrowserRouter>
-  )
-}
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
 //ReactDOM.render(<Router />, document.getElementById('root')); < - not sure why this change, remember for later
 
 // If you want your app to work offline and load faster, you can change
