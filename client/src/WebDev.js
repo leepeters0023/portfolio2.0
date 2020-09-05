03e9947827185aed293a25a17391cfb4a09c88b1
Lee Peters <lpeters0023@gmail.com>
2020-09-05 15:53:12 -0400

    really fixes twinthread image

--- client/src/WebDev.js
+++ client/src/WebDev.js
@@ -26,24 +26,68 @@ class WebDev extends React.Component {
     return (
       <div id='web-dev-container'>
         <div id='web-dev'>
-        <img id='baf453d4db5947d5b653480175c4c934' alt='twinthread-pricing-thumb' className='webdev-thumb' onClick={this.openModal} src='Images/twinthreadPricing.png'></img>
-        <div className='item-description'><h4>TwinThread.com pricing page and submission form</h4></div>
-        <img id='96320ec895dd4060b77559a1cb3de073' alt='portfolio-thumb' className='webdev-thumb' onClick={this.openModal} src='Images/portfolio-thumb.png'></img>
-        <div className='item-description'><h4>A video walk through of THIS website</h4></div>
-        <img id='87b0054d983542baa7735240d057593f' alt='park-burlington-thumb' className='webdev-thumb' onClick={this.openModal} src='Images/parkBTV.png'></img>
-        <h4>Re-designing Burlington Vermont's parking interface with<br></br> JavaScript and Google Maps<br></br><a id='park-btv-link' href='https://park-burlington.herokuapp.com/'>See it live here!</a></h4>
-          <img id='e7754b14c80249eaaa0844afea3f4c84' alt='yelpington-thumb' className='webdev-thumb' onClick={this.openModal} src='Images/yelpington.png'></img>
+          <img 
+          id='baf453d4db5947d5b653480175c4c934' 
+          alt='twinthread-pricing-thumb' 
+          className='webdev-thumb'
+          onClick={this.openModal} 
+          src='Images/twinthreadPricing.png'>
+          </img>
+          <div className='item-description'>
+            <h4>TwinThread.com pricing page and submission form</h4>
+          </div>
+
+          <img 
+            id='96320ec895dd4060b77559a1cb3de073' 
+            alt='portfolio-thumb'  
+            className='webdev-thumb' 
+            onClick={this.openModal} 
+            src='Images/portfolio-thumb.png'>
+          </img>
+          <div className='item-description'><h4>A video walk through of THIS website</h4>
+          </div>
+
+          <img 
+          id='87b0054d983542baa7735240d057593f' 
+          alt='park-burlington-thumb'  
+          className='webdev-thumb' 
+          onClick={this.openModal} 
+          src='Images/parkBTV.png'>
+          </img>
+          <h4>Re-designing Burlington Vermont's parking interface with<br></br> JavaScript and  Google Maps<br></br><a id='park-btv-link' href='https://park-burlington.herokuapp.com/ '>See it live here</a></h4>
+
+          <img 
+          id='e7754b14c80249eaaa0844afea3f4c84' 
+          alt='yelpington-thumb'  
+          className='webdev-thumb' 
+          onClick={this.openModal} 
+          src='Images/yelpington.png'>
+          </img>
           <h4>Geo Locating Burlington, VT businesses with React</h4>
-          <img id='5666b343d9bc43c892735bc1c6a53e8d' alt='tic-tac-toe-thumb' className='webdev-thumb' onClick={this.openModal} src='Images/TicTac.png'></img>
+
+          <img 
+          id='5666b343d9bc43c892735bc1c6a53e8d' 
+          alt='tic-tac-toe-thumb' 
+          className='webdev-thumb' 
+          onClick={this.openModal} 
+          src='Images/TicTac.png'>
+          </img>
           <h4>Classic Tic Tac Toe in JavaScript</h4>
-          <img id='171415a1c6d745d39dda2471215219cb' alt='geo-vermonter-thumb' className='webdev-thumb' onClick={this.openModal} src='Images/geoVT.png'>
+
+          <img 
+          id='171415a1c6d745d39dda2471215219cb' 
+          alt='geo-vermonter-thumb' 
+          className='webdev-thumb' 
+          onClick={this.openModal} 
+          src='Images/geoVT.png'>
           </img>
           <h4>Geo location guessing game in JavaScript</h4>
+
           <div className='more-to-come'>
           <h3 id='more-on-the-way'>More on the way, stay tuned...</h3>
           </div>
-        </div>
-        {this.state.modalDisplay ? <Modal closeModal={this.closeModal} currentModal={this.state.currentModal} /> : null}
+          </div>
+          {this.state.modalDisplay ? <Modal closeModal={this.closeModal} currentModal={this.state.  currentModal} /> : null}
       </div>
     )
   }
